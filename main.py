ef7857378aaa75ce49a0149a3e6709f9ce7ecdd3
Brian <gitlab@selfdestroyedindustries.com>
2022-04-24 13:02:14 -0400

    Adding directory input and exception handling.

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from rich.console import Console
 from rich.markdown import Markdown
 import os
 
+
 console = Console()
 path = input("Please enter the file path: ")
-files = os.scandir(path)
-
-for file in files:
-    with open(file, 'r') as json_file:
-        print(f"\n\nReading: {file}")
-        json_data = json.load(json_file)
-        with open(f"output/{file.name}.txt", 'w') as new_file:
-            try:
-                console.print(Markdown(json_data['title']))
-                new_file.write(json_data['title'])
-                console.print(Markdown(json_data['content']))
-                new_file.write(json_data['content'])
-            except KeyError:
-                pass
-            new_file.close()
+try:
+    files = os.scandir(path)
+    for file in files:
+        with open(file, 'r') as json_file:
+            print(f"\n\nReading: {file}")
+            json_data = json.load(json_file)
+            with open(f"output/{file.name}.txt", 'x') as new_file:
+                try:
+                    console.print(Markdown(json_data['title']))
+                    new_file.write(json_data['title'])
+                    console.print(Markdown(json_data['content']))
+                    new_file.write(json_data['content'])
+                except KeyError:
+                    pass
+                new_file.close()
+except FileNotFoundError:
+    print("Path not found. Bailing.")
