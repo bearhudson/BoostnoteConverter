806ee5a718eac68e1bbbf0493c99d3f225780a4e
Brian <gitlab@selfdestroyedindustries.com>
2022-04-24 10:59:58 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+from rich.console import Console
+from rich.markdown import Markdown
+import os
+
+console = Console()
+path = input("Please enter the file path: ")
+files = os.scandir(path)
+
+for file in files:
+    with open(file, 'r') as json_file:
+        print(f"\n\nReading: {file}")
+        json_data = json.load(json_file)
+        with open(f"output/{file.name}.txt", 'w') as new_file:
+            try:
+                console.print(Markdown(json_data['title']))
+                new_file.write(json_data['title'])
+                console.print(Markdown(json_data['content']))
+                new_file.write(json_data['content'])
+            except KeyError:
+                pass
+            new_file.close()
+cd
